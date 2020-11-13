34079112a413fbe1f95430b729cf7bee7f6d620e
Astral Gnome <reedkeenan@gmail.com>
2020-11-12 22:12:43 -0800

    printed movie title, year, plot, rated, ratings to both local storage and the page.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,13 +1,39 @@
+document.body.innerHTML = `<h1 id="movieTitle"></h1>
+<h1 id="plot"><h1 id="year"><h1 id="rated">
+</h1><h1 id="metacritic"><h1 id="rating"></h1>`
 
-// var movie = "Gremlins";
 var queryURL = "https://www.omdbapi.com/?t=Gremlins&apikey=trilogy";
 
 $.ajax({
-  url: queryURL,
-  method: "GET",
+    url: queryURL,
+    method: "GET",
 }).then(function (response) {
-  console.log(response);
+    console.log(response);
+    
+    //Below ID tags are just placeholders. My hope is that we will be able to swap
+    //these out with their relevant counterparts on the Front End.
+    var movieTitle = $("#movieTitle").text(response.Title)
+    var year = $("#year").text(response.Year)
+    var plot = $("#plot").text(response.Plot)
+    var rated = $("#rated").text(response.Rated)
+    var metacritic = $("#metacritic").text(response.Ratings[1].Source)
+    var ratings = $("#rating").text(response.Ratings[1].Value)
+    
+    localStorage.setItem("title", JSON.stringify(response.Title))
+    localStorage.setItem("year", JSON.stringify(response.Year))
+    localStorage.setItem("plot", JSON.stringify(response.Plot))
+    localStorage.setItem("rated", JSON.stringify(response.Title))
+    localStorage.setItem("metacritic", JSON.stringify(response.Ratings[1].Source))
+    localStorage.setItem("rating", JSON.stringify(response.Ratings[1].Value))
+    
+    console.log()
+
 });
+
+function createItem() {
+    
+}
+
 
 //$("#find-book").on("click", function(event) {
   //  event.preventDefault();
@@ -20,7 +46,3 @@ $.ajax({
         console.log(response);
     })
 //});
-
-
-);
-});
