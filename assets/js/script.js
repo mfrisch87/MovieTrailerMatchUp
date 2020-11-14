82f2217e52e1fcea4823b0b5bbe03ca0c29802ae
mfrisch87 <mikey.frisch@gmail.com>
2020-11-14 10:35:50 -0800

    youtube iframe data

--- assets/js/script.js
+++ assets/js/script.js
@@ -10,13 +10,58 @@ $.ajax({
 });
 
 // Youtube Search to find Trailer
-   var videoTitle = "Arrival Trailer";
+   var videoTitle = movieTitle +  "Official trailer" + year;
     var videoQueryURL = "https://youtube.googleapis.com/youtube/v3/search?q=" + videoTitle + "&type=video&chart=mostPopular&key=AIzaSyCkifdAmM0IIjs8znt7RH2_-_6o4xjOPYs"
     $.ajax({
         url: videoQueryURL,
         method: "GET"
     }).then(function(response) {
+      var trailerId = response.items[0].id.videoId;
+  
         console.log(response);
+        console.log(trailerId);
+        
     })
 
 
+// IFrame API
+var tag = document.createElement('script');
+
+      tag.src = "https://www.youtube.com/iframe_api";
+      var firstScriptTag = document.getElementsByTagName('script')[0];
+      firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+
+
+  //  This function creates an <iframe> (and YouTube player)
+      //    after the API code downloads.
+      var player;
+      function onYouTubeIframeAPIReady() {
+        player = new YT.Player('player', {
+          height: '390',
+          width: '640',
+          videoId: trailerId,
+          events: {
+            'onReady': onPlayerReady,
+            'onStateChange': onPlayerStateChange
+          }
+        });
+      }
+
+ // The API will call this function when the video player is ready.
+ function onPlayerReady(event) {
+  event.target.playVideo();
+}
+
+// The API calls this function when the player's state changes.
+//    The function indicates that when playing a video (state=1),
+//    the player should play for six seconds and then stop.
+var done = false;
+function onPlayerStateChange(event) {
+  if (event.data == YT.PlayerState.PLAYING && !done) {
+    setTimeout(stopVideo, 6000);
+    done = true;
+  }
+}
+function stopVideo() {
+  player.stopVideo();
+}
