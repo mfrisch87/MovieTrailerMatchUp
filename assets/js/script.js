cc5696ae4d4909a6b008ab307b2d8fe20e9c01d9
Astral Gnome <reedkeenan@gmail.com>
2020-11-14 23:40:22 -0800

    responsivenessness

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,7 +2,7 @@
 // <h1 id="plot"></h1><h1 id="year"></h1><h1 id="rated"></h1>
 // <h1 id="rottenTomatoes"></h1><h1 id="rating"></h1>`
 
-var parentButtonEl = $("#parent-button")
+var posterDisplayEl = $("#poster-display")
 
 searchMovie()
 function searchMovie() {
@@ -22,24 +22,21 @@ function searchMovie() {
     for (i = 0; i < 8; i++){
     
     movieCollection[i] = response.Search[i];
-    
+    console.log(movieCollection[i].Poster)
+    }
+    displayPosters(movieCollection);
+    console.log(movieCollection)
+});
 }
-
-parentButtonEl.innerHTML =
-`<p class="level-item">
-<button class="button is-small has-text-primary is-rounded" data-index="0">${movieCollection[0].Title}</button></p>
-<p class="level-item">
-<button class="button is-small has-text-primary is-rounded" data-index="1">${movieCollection[1].Title}</button></p>
-<p class="level-item">
-<button class="button is-small has-text-primary is-rounded" data-index="2">${movieCollection[2].Title}</button></p>
-<p class="level-item">
-<button class="button is-small has-text-primary is-rounded" data-index="3">${movieCollection[3].Title}</button></p>
-<p class="level-item">
-<button class="button is-small has-text-primary is-rounded" data-index="4">${movieCollection[4].Title}</button></p>`
+function displayPosters (movieCollection) {
+//corrected ID spelling below to "firstBlock" rather than "firstblock"
+console.log(movieCollection)
+posterDisplayEl.innerHTML =
+`<section id="firstBlock" class="section columns"><section class="column is-one-fourth-desktop is-one-fourth-desktop image"><img class="has-ratio" src="${movieCollection[0].Poster}"><p id="title-and-year" class="text-is">Superman 1998</p></section><section class="column is-one-fourth-desktop image"><img class="has-ratio" src="${movieCollection[1].Poster}"></section><section class="column is-one-fourth-desktop image"><img class="has-ratio" src="${movieCollection[2].Poster}"></section><section class="column is-one-fourth-desktop image"><img class="has-ratio" src="${movieCollection[3].Poster}"></section></section><section id="secondBlock" class="section columns"><section class="column is-one-fourth-desktop image"><img class="has-ratio" src="${movieCollection[4].Poster}"></section><section class="column is-one-fourth-desktop image"><img class="has-ratio" src="${movieCollection[5].Poster}"></section><section class="column is-one-fourth-desktop image"><img class="has-ratio" src="${movieCollection[6].Poster}"></section><section class="column is-one-fourth-desktop image"><img class="has-ratio" src="${movieCollection[7].Poster}"></section></section>`
 
 localStorage.setItem("movieCollectionArray", JSON.stringify(movieCollection))//How do we parse the imdbID out of this?
 
-
+}
 
     // var poster = response.Poster
     // var movieTitle = response.Title
@@ -57,73 +54,73 @@ localStorage.setItem("movieCollectionArray", JSON.stringify(movieCollection))//H
     // localStorage.setItem("rottenTomatoes", JSON.stringify(response.Ratings[1].Source))
     // localStorage.setItem("rating", JSON.stringify(response.Ratings[1].Value))
     
-    var yearOfRelease = response.Year
+    // var yearOfRelease = response.Year
     
     
 
     
 
-    });
-}
+    
 
-searchMovie("Goonies")
 
-// Youtube Search to find Trailer
-   var videoTitle = movieTitle +  "Official trailer" + year;
-    var videoQueryURL = "https://youtube.googleapis.com/youtube/v3/search?q=" + videoTitle + "&type=video&chart=mostPopular&key=AIzaSyCkifdAmM0IIjs8znt7RH2_-_6o4xjOPYs"
-    $.ajax({
-        url: videoQueryURL,
-        method: "GET"
-    }).then(function(response) {
-      var trailerId = response.items[0].id.videoId;
+// searchMovie("Goonies")
+
+// // Youtube Search to find Trailer
+//    var videoTitle = movieTitle +  "Official trailer" + year;
+//     var videoQueryURL = "https://youtube.googleapis.com/youtube/v3/search?q=" + videoTitle + "&type=video&chart=mostPopular&key=AIzaSyCkifdAmM0IIjs8znt7RH2_-_6o4xjOPYs"
+//     $.ajax({
+//         url: videoQueryURL,
+//         method: "GET"
+//     }).then(function(response) {
+//       var trailerId = response.items[0].id.videoId;
   
-        console.log(response);
-        console.log(trailerId);
+//         console.log(response);
+//         console.log(trailerId);
         
-    })
+//     })
 
 
-// IFrame API
-var tag = document.createElement('script');
+// // IFrame API
+// var tag = document.createElement('script');
 
-      tag.src = "https://www.youtube.com/iframe_api";
-      var firstScriptTag = document.getElementsByTagName('script')[0];
-      firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+//       tag.src = "https://www.youtube.com/iframe_api";
+//       var firstScriptTag = document.getElementsByTagName('script')[0];
+//       firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
 
 
   //  This function creates an <iframe> (and YouTube player)
-      //    after the API code downloads.
-      var player;
-      function onYouTubeIframeAPIReady() {
-        player = new YT.Player('player', {
-          height: '390',
-          width: '640',
-          videoId: trailerId,
-          events: {
-            'onReady': onPlayerReady,
-            'onStateChange': onPlayerStateChange
-          }
-        });
-      }
+//       //    after the API code downloads.
+//       var player;
+//       function onYouTubeIframeAPIReady() {
+//         player = new YT.Player('player', {
+//           height: '390',
+//           width: '640',
+//           videoId: trailerId,
+//           events: {
+//             'onReady': onPlayerReady,
+//             'onStateChange': onPlayerStateChange
+//           }
+//         });
+//       }
 
- // The API will call this function when the video player is ready.
- function onPlayerReady(event) {
-  event.target.playVideo();
-}
+//  // The API will call this function when the video player is ready.
+//  function onPlayerReady(event) {
+//   event.target.playVideo();
+// }
 
-// The API calls this function when the player's state changes.
-//    The function indicates that when playing a video (state=1),
-//    the player should play for six seconds and then stop.
-var done = false;
-function onPlayerStateChange(event) {
-  if (event.data == YT.PlayerState.PLAYING && !done) {
-    setTimeout(stopVideo, 6000);
-    done = true;
-  }
-}
-function stopVideo() {
-  player.stopVideo();
-}
+// // The API calls this function when the player's state changes.
+// //    The function indicates that when playing a video (state=1),
+// //    the player should play for six seconds and then stop.
+// var done = false;
+// function onPlayerStateChange(event) {
+//   if (event.data == YT.PlayerState.PLAYING && !done) {
+//     setTimeout(stopVideo, 6000);
+//     done = true;
+//   }
+// }
+// function stopVideo() {
+//   player.stopVideo();
+// }
 
 
 $("#search").on("click", searchMovie)
