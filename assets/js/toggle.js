f146ab421a730d65ae26aed3e29818c9dc143c1a
daniel <danielsoledad@gmail.com>
2020-11-14 20:53:02 -0800

    adding burger nav to project

---
+++ assets/js/toggle.js
@@ -0,0 +1,6 @@
+$(document).ready(function(){
+    $(".navbar-burger").click(function(){
+        $('#navbarToggle').toggleClass('is-active')
+        $('.navbar-burger').toggleClass('is-active')
+    })
+})
