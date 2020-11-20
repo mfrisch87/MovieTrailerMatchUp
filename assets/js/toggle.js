06d5adf85a0080d6f2ff849712a1680c94761547
daniel <danielsoledad@gmail.com>
2020-11-20 13:46:53 -0800

    adding my key

--- assets/js/toggle.js
+++ assets/js/toggle.js
@@ -1,18 +1,12 @@
 
 //API Key Array
 ytAPIKeys ={ 
-    yKeys:[
-      'AIzaSyAg2pgaj4to8E_XV8x1Icu_THjtxNCMTM0',
-      'AIzaSyDeTVXmZHeduetXTmG4T4jTkkot-ioAbV4',
-      'AIzaSyB-WiR-JT5QTEr2GzOkib58IDNpdF37HdA',
-      'AIzaSyBse8dtbY4ZCzX5LrgLJSq-aHl4pNYDeH8',
-      'AIzaSyD4Tthiiwiyiz44GjXJOC4bdi0GkMYGF-U',
-      'AIzaSyABwvhTHaQmSTCnVCnByAw0PQbvQGUOT-M',
-      'AIzaSyCkifdAmM0IIjs8znt7RH2_-_6o4xjOPYs',
-      'AIzaSyDgv-l3BTAuJoboL7mU6jt28CQlD2XaE64',
-      'AIzaSyAP02SCrNR6xtWnk25HDQPOxeX4gbU7kCo'
-    ],
-  mKey:'c2e8eb3c'
+    yKeys:
+    "AIzaSyADtyGp19GrSaE4roaX6YHv_BTfM8YsfCI"
+    ,
+    mKey:'c2e8eb3c'
 }
- 
+
+
+
 
