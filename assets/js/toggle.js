f992add7057353cb801d2e2bb8468ad61ca5ced7
daniel <danielsoledad@gmail.com>
2020-11-19 22:36:32 -0800

    added keys to seperate file

--- assets/js/toggle.js
+++ assets/js/toggle.js
@@ -1,1 +1,15 @@
-var movieArray = ["spider man", "black panther", "iron man","street fighter", ];
+
+
+ytAPIKeys ={ 
+    ykeys:[
+    'AIzaSyAg2pgaj4to8E_XV8x1Icu_THjtxNCMTM0',
+    'AIzaSyDeTVXmZHeduetXTmG4T4jTkkot-ioAbV4',
+    'AIzaSyB-WiR-JT5QTEr2GzOkib58IDNpdF37HdA',
+    'AIzaSyBse8dtbY4ZCzX5LrgLJSq-aHl4pNYDeH8',
+    'AIzaSyD4Tthiiwiyiz44GjXJOC4bdi0GkMYGF-U',
+    'AIzaSyABwvhTHaQmSTCnVCnByAw0PQbvQGUOT-M'
+  ],
+  mKey:'c2e8eb3c'
+}
+ 
+
